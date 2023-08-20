9870fbe4b89e79f824d3ea84ba5bface16e3071a
renerodrigues01 <119670685+renerodrigues01@users.noreply.github.com>
2023-08-20 22:20:50 +0000

    new

--- start/routes.ts
+++ start/routes.ts
@@ -19,51 +19,5 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
-import AdminController from 'App/Controllers/Http/AdminsController'
-import ApplicationController from 'App/Controllers/Http/ApplicationController'
-import AuthController from 'App/Controllers/Http/AuthController'
 
 Route.get('/', async ({ view }) => view.render('home'))
-Route.get('/ask-for-loan', async ({ view }) => view.render('ask-for-loan'))
-Route.get('/services', async ({ view }) => view.render('services'))
-Route.get('/terms-conditions', async ({ view }) => view.render('terms-conditions'))
-Route.get('/privacy-policy', async ({ view }) => view.render('privacy-policy'))
-
-Route.get('/login', async ({ view, auth }) => view.render(auth.isLoggedIn ? 'dashboard' : 'login'))
-Route.post('/login', AuthController.login)
-
-Route.group(() => {
-  Route.get('/', ApplicationController.index).middleware('admin')
-  Route.post('/', ApplicationController.store)
-  Route.get('/:id', ApplicationController.show).middleware('admin')
-  Route.put('/:id', ApplicationController.update).middleware('admin')
-  Route.delete('/:id', ApplicationController.destroy).middleware('admin')
-}).prefix('/applications')
-
-Route.group(() => {
-  Route.get('/dashboard', async ({ view, auth }) =>
-    view.render('dashboard', {
-      user: auth.user,
-      applications: await auth.user?.related('applications').query()
-    })
-  )
-  Route.get('/logout', AuthController.logout)
-}).middleware('auth')
-
-Route.get('/success', async ({ view }) => view.render('success'))
-
-Route.get('/unauthorized', async ({ view }) => view.render('unauthorized'))
-
-Route.group(() => {
-  Route.get('/admin/application/:id', AdminController.show)
-  Route.get('/admin/application/:id/delete', AdminController.destroy)
-  Route.post('/admin/application/:id', AdminController.update)
-  Route.get('/admin/dashboard', AdminController.index)
-  Route.get('/admin/logout', AuthController.adminLogout)
-}).middleware('admin')
-
-Route.post('/admin/login', AdminController.login)
-
-Route.get('admin/login', async ({view}) => view.render('admin/login'))
-
-Route.get('admin', async ({ view, auth }) => view.render((auth.user?.role === "admin") ? 'admin/dashboard' : 'admin/login'))
