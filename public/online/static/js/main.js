01f6c184ff9645cdafc636682a6e347e5ffeb435
renerodrigues01 <119670685+renerodrigues01@users.noreply.github.com>
2023-08-28 15:28:45 +0000

    new

--- public/online/static/js/main.js
+++ public/online/static/js/main.js
@@ -72,3 +72,39 @@ inputs.forEach(function (input) {
   })
 })
 
+const form = document.getElementById("my-form");
+
+const statusResult = document.getElementById("")
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  const data = new FormData(event.target);
+
+  const encodedData = JSON.stringify(data);
+
+  fetch(form.action, {
+    method: "POST",
+    body: encodedData,
+    headers: {
+      'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      statusResult.innerHTML = "Thanks for your submission!";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          statusResult.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          statusResult.innerHTML = "Oops! There was a problem submitting your form"
+        }
+      })
+    }
+  }).catch(error => {
+    statusResult.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+
+form.addEventListener("submit", handleSubmit)
+
